4adb74b9cc8ce14a2f6d91fbac7044cf859476cd
situ-manandhar-ibm-com <situ.manandhar@ibm.com>
2023-04-12 21:47:28 -0500

    added changes to handle canonical urls, issues with 500s

--- hertz/hertzspider/settings.py
+++ hertz/hertzspider/settings.py
@@ -24,7 +24,7 @@ PLAYWRIGHT_LAUNCH_OPTIONS = {
 # PLAYWRIGHT_BROWSER_TYPE = "firefox"
 # PLAYWRIGHT_DEFAULT_NAVIGATION_TIMEOUT = 50000
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-USER_AGENT = 'None'
+#USER_AGENT = 'None'
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
 
@@ -33,16 +33,22 @@ SCHEDULER_PRIORITY_QUEUE = 'scrapy.pqueues.DownloaderAwarePriorityQueue'
 #CONCURRENT_REQUESTS = 0
 # LOG_ENABLED = True
 REACTOR_THREADPOOL_MAXSIZE = 20
-# LOG_LEVEL = 'DEBUG'
+# while developing we want to see debug logs
+LOG_LEVEL = "DEBUG" # or "INFO" in production
 COOKIES_ENABLED = True
+REQUEST_DEPTH_MAX = 0
 
+# will cache all request to /httpcache directory which makes running spiders in development much quicker
+# tip: to refresh cache just delete /httpcache directory
+#HTTPCACHE_ENABLED = True
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 3
-DOWNLOAD_TIMEOUT = 30
+#DOWNLOAD_DELAY = 3
+#DOWNLOAD_TIMEOUT = 30
 # The download delay setting will honor only one of:
 CONCURRENT_REQUESTS_PER_DOMAIN = 32
+#PLAYWRIGHT_MAX_PAGES_PER_CONTEXT = 4
 # CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
@@ -52,12 +58,14 @@ CONCURRENT_REQUESTS_PER_DOMAIN = 32
 # TELNETCONSOLE_ENABLED = False
 
 # Override the default request headers:
-DEFAULT_REQUEST_HEADERS = {
-    # we should use headers
-    'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36",
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-    'Accept-Language': 'en',
-}
+#DEFAULT_REQUEST_HEADERS = {
+#     # we should use headers
+#    'User-Agent': "hertz-sitemap-builder",
+#     'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36",
+#     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+#     'Accept-Language': 'en'
+#     'x-irac-bot-access':'true'
+#}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://doc.scrapy.org/en/latest/topics/autothrottle.html
